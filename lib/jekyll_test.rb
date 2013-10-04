87a7eded9d10f633fddbe8ff218e6da154b7107a
Parker Moore <parkrmoore@gmail.com>
2013-10-04 15:37:07 -0400

    Implement

--- lib/jekyll_test.rb
+++ lib/jekyll_test.rb
@@ -1,5 +1,21 @@
 require "jekyll_test/version"
+require "jekyll"
 
 module JekyllTest
-  # Your code goes here...
+  class TestPage < Jekyll::Page
+    def initialize(site, base, dir, name)
+      @site = site
+      @base = base
+      @dir  = dir
+      @name = name
+      self.process(name)
+      self.content = "this is a test"
+    end
+  end
+
+  class TestGenerator < Jekyll::Generator
+    def generate(site)
+      site.pages << Page.new(site, site.source, '', 'test.txt')
+    end
+  end
 end

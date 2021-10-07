9ff00acad8fe445334e15c6daf3c85eeb036784f
Ashwin Maroli <ashmaroli@gmail.com>
2021-10-07 19:48:31 +0530

    Add a workflow script to print details about tag

---
+++ .github/workflows/scripts/tag_info.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require "colorator"
+require "json"
+
+data = JSON.parse(File.binread('event.json'))
+label = %Q(TAG #{data["ref"].delete_prefix("refs/tags/").cyan})
+puts <<-TEXT
+
+  ---------------------------------------------------------
+  #{label.center(64)}
+  ---------------------------------------------------------
+
+
+      Pushed by: #{data.dig("pusher", "name")&.cyan}
+     Repository: #{data.dig("repository", "full_name")&.cyan}
+
+
+    Head Commit Details:
+
+      #{data.dig("head_commit", "message").inspect.cyan}
+
+         Author: #{data.dig("head_commit", "author", "username")&.cyan}
+      Committer: #{data.dig("head_commit", "committer", "username")&.cyan}
+         SHA ID: #{data.dig("head_commit", "id")&.cyan}
+
+  ---------------------------------------------------------
+TEXT
